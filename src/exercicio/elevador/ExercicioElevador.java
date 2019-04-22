8b1ff4161a39e2dc35ed15ac90b6c8b9c6f04026
claudinei <diney_rock19@hotmail.com>
2019-04-21 22:16:05 -0300

    Add menu interativo

--- src/exercicio/elevador/ExercicioElevador.java
+++ src/exercicio/elevador/ExercicioElevador.java
@@ -1,6 +1,7 @@
 package exercicio.elevador;
 
 import exercicio.elevador.Elevador;
+import java.util.Scanner;
 
 /**
  *
@@ -14,15 +15,47 @@ public class ExercicioElevador {
     public static void main(String[] args) {
 
         Elevador e = new Elevador(10, 10);
+        Scanner input = new Scanner(System.in);
+        int opcao = 0;
 
-        e.entra(10);
-        e.sobe(5);
-        e.sai(5);
-        e.sobe(10);
-        e.sai(5);
-        e.entra(2);
-        e.desce(1);
-        e.sai(2);
+        do {
+
+            System.out.println("|------------------------------|");
+            System.out.println("|***Escolha a opção desejada***|");
+            System.out.println("|------------------------------|");
+            System.out.println("|1 - Adicionar pessoas         |");
+            System.out.println("|2 - Remover pessoas           |");
+            System.out.println("|3 - subir                     |");
+            System.out.println("|4 - descer                    |");
+            System.out.println("|------------------------------|");
+
+            System.out.println("O que deseja fazer?");
+            opcao = (input.nextInt());
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Informe a quantidade de pessoas vão entrar no elevador.");
+                    e.entra(input.nextInt());
+                    break;
+                case 2:
+                    System.out.println("Informe a quantidade de pessoas vão sair do elevador.");
+                    e.sai(input.nextInt());
+                    break;
+                case 3:
+                    System.out.println("Informe qual o andar.");
+                    e.sobe(input.nextInt());
+                    break;
+                case 4:
+                    System.out.println("Informe qual o andar.");
+                    e.desce(input.nextInt());
+                    break;
+                case 0:
+                    opcao = 0;
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        } while (opcao != 0);
     }
 
 }
